52a5177865e0b6b612b94781c9e67e45a1cda9ab
quyen <chibedethuong91@gmail.com>
2017-09-24 10:27:06 +0700

    Add migration + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  api_subdomain = 'api'
+  constraints subdomain: api_subdomain do
+
+    namespace :api, path: nil do
+      namespace :v1 do
+        resources :users, only: [:index] do
+          collection do
+            post :register
+          end
+        end
+        resources :posts, only: [:index, :create, :show] do
+          member do
+            resources :comments, only: [:index, :create]
+          end
+        end
+      end
+    end
+
+  end
 end
