37796697d1cf09560347ed2233cb89177e8fcf54
quyen <chibedethuong91@gmail.com>
2017-09-24 13:31:23 +0700

    fix heroku subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if Rails.env.production?
-    api_subdomain = ''
-  else
-    api_subdomain = 'api'
-  end
-  constraints subdomain: api_subdomain do
-
-    namespace :api, path: nil do
+  # For details on the DSL available within   constraints subdomain: api_subdomain do
+    namespace :api do
       namespace :v1 do
         resources :users, only: [:index] do
           collection do
@@ -26,6 +19,4 @@ Rails.application.routes.draw do
         end
       end
     end
-
-  end
 end
